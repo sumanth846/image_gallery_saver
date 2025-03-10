065a1d44734c99434150568a72a51871b86dadd5
suamntha <sumanth@zendynamix.com>
2025-03-10 21:47:36 +0530

    Update: Remove unused import statement `//import io.flutter.plugin.common.MethodChannel.Result` in ImageGallerySaverPlugin.kt

--- android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
+++ android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
@@ -17,7 +17,7 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-//import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.MethodChannel.Result
 //import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.File
 import java.io.FileInputStream
